df1b97150782a6b8e221921ceba8e2d342d8b4cc
madiha lali <madiha.lali@gmail.com>
2023-04-21 07:25:00 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-	char ch;
+	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
 		putchar(ch);
-	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
 	putchar('\n');
 	return (0);
 }

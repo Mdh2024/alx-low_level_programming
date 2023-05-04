aaa39b8d8c81d6cc71239e732c1767faa7147e1a
madiha lali <madiha.lali@gmail.com>
2023-05-04 11:55:49 -0700

    magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  * main - check the code
  *
@@ -20,7 +20,7 @@ p = &n;
    * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement.
-   * - you are not allowed to code anything else than this line of code
+   * - you are  not allowed to code anything else than this line of code
    */
 *(p + 5) = 98;
   /* ...so that this prints 98\n */
